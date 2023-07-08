d6371bb1cdbde00c0d8556ea665a3cfb9ca649f0
sweetcter <daylasep0175@gmail.com>
2023-07-08 15:53:07 +0700

    update slide and paging

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,7 +2,7 @@ let filterCategory = document.querySelectorAll(".filter-category");
 
 for (let i = 0; i < filterCategory.length; i++) {
   filterCategory[i].onclick = function (e) {
-    console.log('Button clicked');
+    console.log("Button clicked");
     if (e.target.closest(".active")) {
       filterCategory[i].classList.remove("active");
     } else {
@@ -10,3 +10,18 @@ for (let i = 0; i < filterCategory.length; i++) {
     }
   };
 }
+// bắt sự kiện onscoll ở trình duyệt để hiển thị menu
+function hiddenMenubar() {
+  document.onscroll = function () {
+    const scollTop = window.scrollY || document.documentElement.scrollTop;
+    const header = document.querySelector(".header");
+    if (scollTop >= 200) {
+      header.classList.add("nav-active");
+      header.style.justifyContent =`space-between`;
+    } else {
+      header.classList.remove("nav-active");
+      header.style.justifyContent =``;
+    }
+  };
+}
+hiddenMenubar();
