4ffb2ab5e24eac725cc7cd0ccf64e4e13d847912
cuongfpt <90626058+cuongfpt@users.noreply.github.com>
2023-08-05 01:46:28 +0700

    binh luan

--- includes/home2.php
+++ includes/home2.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
-
+  <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -20,6 +20,7 @@
   <link rel="stylesheet" href="../<?= $ROOTt_URL ?>/asset/css/index.css" />
   <link rel="stylesheet" href="../<?= $ROOTt_URL ?>/asset/css/spct.css" />
   <link rel="stylesheet" href="../<?= $ROOTt_URL ?>/asset/css/user.css" />
+  <link rel="stylesheet" href="../<?= $ROOTt_URL ?>/asset/css/comment.css" />
 
 </head>
 
