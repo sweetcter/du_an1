946058a248eb7d67df7f18768d7cc21e332d8ab7
duongtrongbinh <137783978+duongtrongbinh@users.noreply.github.com>
2023-09-03 15:50:16 +0700

    them

---
+++ asset/js/validate.js
@@ -0,0 +1,14 @@
+// phàn validate 
+// validate trống
+// validate email 
+// validate password
+// validate phần name ,full name lớn hơn 3 nhỏ hơn 30
+// validate dung lượng của 1 file khi gửi đi 
+// validate phần admin và user
+
+
+
+// phần hiện cửa sở lỗi hay không l
+
+
+
